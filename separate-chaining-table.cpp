42e38ec0eaf75e4dbd482d842016aaf46a9a00d7
SHIMANOEYE <shimanoee@yandex.ru>
2024-02-10 18:01:27 +0300

    some fixes

--- separate-chaining-table.cpp
+++ separate-chaining-table.cpp
@@ -11,14 +11,13 @@ void SeparateChainingTable::insert(key_t key, val_t val) {
     int index = hash(key);
     std::list<data_t> temp = cells[index];
 
-    data_t elem{key, val};
-
     std::list<data_t>::iterator found_it =
-        std::find_if(temp.end(), temp.end(),
+        std::find_if(temp.begin(), temp.end(),
                      [key](data_t &data) { return data.key == key; });
 
-    if (found_it == temp.end())
+    if (found_it == temp.end()) {
+      data_t elem{key, val};
       temp.push_back(elem);
-    else
+    } else
       found_it->val = val;
 }
--- source.cpp
+++ source.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "hash-table.h"
+#include "separate-chaining-table.h"
 
 using namespace std;
 
-
-
 int main() {
     cout << "123";
-
 }
